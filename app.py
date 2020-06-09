e4e1da4b9a0783c41e2aad2a47e813d5e78eafd2
Pongpichat Sumetha <55397104+pongpichat2@users.noreply.github.com>
2020-06-10 03:20:57 +0700

    day 3

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ url = "https://project-e54fe.firebaseio.com/"
 firebase = firebase.FirebaseApplication(url, authentication = None)
 
 
-# db = firebase.get('/messages', None)
-
-
-
 app = Flask(__name__,template_folder='template')
 
 @app.route("/Addchoice") #URL
@@ -43,13 +39,36 @@ def insertChoice():
 
         Avgtime = Cal.Calchoice(Answer)
 
-        countDataQuiz = str(len(firebase.get('/Quiz', None))+1)
+        # countDataQuiz = str(len(firebase.get('/Quiz', None))+1)
         Insert = {'Subject':subject,'QuizName':Propo,'Ch1':choice1,'Ch2':choice2,'Ch3':choice3,'Ch4':choice4,'Answer':Answer,'Time':Avgtime}
 
-        result = firebase.post('Quiz/'+countDataQuiz,Insert)
+        # result = firebase.post('Quiz/'+countDataQuiz,Insert)
+        result = firebase.post('Quiz/',Insert)
 
         return render_template('Addchoice.html')
 
+@app.route("/AddMember")
+def PageAddMem():
+    return render_template('AddMember.html')
+
+@app.route("/insertMember", methods=['GET','POST'])
+def insertMember():
+    if request.method == "POST":
+        fname = request.form['Fname']
+        lname = request.form['Lname']
+        ProSub = request.form['ProSubject']
+        Username = request.form['Username']
+        password = request.form['Password']
+        Conpass = request.form['ComPass']
+        email = request.form['Email']
+        tel = request.form['Tel']
+
+        Insert = {'Firstname':fname,'Lname':lname,'ProSubject':ProSub,'Username':Username,
+        'Password':password,'ConfirmPass':Conpass,'Email':email,'Tel':tel}
+
+        execute = firebase.post('Member/',Insert)
+
+        return render_template('AddMember.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/addMem2.js
@@ -0,0 +1,52 @@
+
+function CheckDatanull() {
+    var username = document.forms["AddMember"]["Username"].value;
+    var Password = document.forms["AddMember"]["Password"].value;
+    var fname = document.forms["AddMember"]["Fname"].value;
+    var lname = document.forms["AddMember"]["Lname"].value;
+    var Compass = document.forms["AddMember"]["ComPass"].value;
+    var email = document.forms["AddMember"]["Email"].value;
+    var tel = document.forms["AddMember"]["Tel"].value;
+    if (fname == "" || fname == null) {
+        alert("กรุณากรอก First ด้วยครับ!! ");
+        document.getElementById("Fname").focus();
+        return false;
+    }
+    if (lname == "" || lname == null) {
+        alert("กรุณากรอก Lastname ด้วยครับ!! ");
+        document.getElementById("Lname").focus();
+        return false;
+    }
+    if (username == "" || username == null) {
+        alert("กรุณากรอก Username ด้วยครับ!! ");
+        document.getElementById("Username").focus();
+        return false;
+    }
+    if (Password == "" || Password == null) {
+        alert("กรุณากรอก Password ด้วยครับ!! ");
+        document.getElementById("Password").focus();
+        return false;
+    }
+    
+    if (Compass == "" || Compass == null) {
+        alert("กรุณากรอก Co-Password ด้วยครับ!! ");
+        document.getElementById("ComPass").focus();
+        return false;
+    }
+    if (email == "" || email == null) {
+        alert("กรุณากรอก E-mail ด้วยครับ!! ");
+        document.getElementById("Email").focus();
+        return false;
+    }
+    if (tel == "" || tel == null) {
+        alert("กรุณากรอก Tel ด้วยครับ!! ");
+        document.getElementById("Tel").focus();
+        return false;
+    }
+
+    if(Password != Compass) {
+        alert("Password และ ConfirmPassword ไม่ตรงกันครับ");
+        document.getElementById("ComPass").focus();
+        return false;
+    }
+}
